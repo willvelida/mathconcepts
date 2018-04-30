80bc3f6b10c3ab78bc572c744e7f0d5ed56f0e63
Will Velida <willvelida@hotmail.co.uk>
2018-04-30 19:39:48 +1200

    Implementing magnitude and direction calculations.

--- vector.py
+++ vector.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -35,6 +37,20 @@ class Vector(object):
         new_coordinates = [c*x for x in self.coordinates]
         return Vector(new_coordinates)
 
+    # Get the magnitude of two vectors
+    def magnitude(self):
+        coordinates_squared = [x**2 for x in self.coordinates]
+        return sqrt(sum(coordinates_squared))
+
+    # Normalize a vector
+    def normalized(self):
+        try:
+            magnitude = self.magnitude()
+            return self.times_scalar(1./magnitude)
+        
+        except ZeroDivisionError:
+            raise Exception('Cannot normalize the zero vector')
+
 my_vector = Vector([1,2,3])
 print(my_vector)
 my_vector2 = Vector([1,2,3])
@@ -42,17 +58,32 @@ my_vector3 = Vector([-1,2,3])
 print(my_vector == my_vector2)
 print(my_vector2 == my_vector3)
 
-# Quiz 
+# adding two vectors
 v = Vector([8.218,-9.341])
 w = Vector([-1.129,2.111])
 print(v.plus(w))
 
+# subtracting two vectors
 v = Vector([7.119,8.215])
 w = Vector([-8.223,0.878])
 print(v.minus(w))
 
+# scaling a vector
 v = Vector([1.671,-1.012,-0.318])
 c = 7.41
 print(v.times_scalar(c))
+
+# Magnitude result
+v = Vector([-0.221,7.437])
+print(v.magnitude())
+
+v = Vector([8.813, -1.331, -6.247])
+print(v.magnitude())
+
+v = Vector([5.581, -2.136])
+print(v.normalized())
+
+v = Vector([1.996, 3.108, -4.554])
+print(v.normalized())
 
 
