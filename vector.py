9b2b0fd9c2d0cd80684281b4e9a6991416833abb
Will Velida <willvelida@hotmail.co.uk>
2018-05-01 18:47:35 +1200

    Implementing cross product calculation functionality

--- vector.py
+++ vector.py
@@ -8,6 +8,7 @@ class Vector(object):
     CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
     NO_UNIQUE_PARALLEL_COMPONENT_MSG = 'No unique parallel component'
     NO_UNIQUE_ORTHOGONAL_COMPONENT_MSG = 'No unique orthogonal component'
+    ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG = 'Only defined in two three dimensions'
 
     def __init__(self, coordinates):
         try:
@@ -117,6 +118,36 @@ class Vector(object):
                 raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
             else:
                 raise e
+
+    # Cross function
+    def cross(self, v):
+        try:
+            x_1, y_1, z_1 = self.coordinates
+            x_2, y_2, z_2 = v.coordinates
+            new_coordinates = [ y_1*z_2 - y_2*z_1,
+                                -(x_1*z_2 - x_2*z_1),
+                                x_1*y_2 - x_2*y_1]
+            return Vector(new_coordinates)
+        
+        except ValueError as e:
+            msg = str(e)
+            if msg == 'need more than 2 values to unpack':
+                self_embedded_in_R3 = Vector(self.coordinates + ('0',))
+                v_embedded_in_R3 = Vector(v.coordinates + ('0',))
+                return self_embedded_in_R3.cross(v_embedded_in_R3)
+            elif (msg == 'too many values to unpack' or msg == 'need more than 1 value to unpack'):
+                raise Exception(self.ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG)
+            else:
+                raise e
+    
+    # Find area of triangle
+    def area_of_triangle_width(self, v):
+        return self.area_of_parallelogram_width(v) / 2.0
+
+    # Find the area of the parallelogram
+    def area_of_parallelogram_width(self, v):
+        cross_product = self.cross(v)
+        return cross_product.magnitude()
     
 
 my_vector = Vector([1,2,3])
