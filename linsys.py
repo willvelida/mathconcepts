008caf636523956d1f5b1a8c0ad78c0ef413f162
Will Velida <willvelida@hotmail.co.uk>
2018-05-05 20:21:03 +1200

    Adding row operations to linsys file

--- linsys.py
+++ linsys.py
@@ -27,15 +27,31 @@ class LinearSystem(object):
 
 
     def swap_rows(self, row1, row2):
-        pass # add your code here
+        self[row1], self[row2] = self[row2], self[row1]
 
 
     def multiply_coefficient_and_row(self, coefficient, row):
-        pass # add your code here
+        n = self[row].normal_vector
+        k = self[row].constant_term
+
+        new_normal_vector = n.times_scalar(coefficient)
+        new_constant_term = k * coefficient
+
+        self[row] = Plane(normal_vector=new_normal_vector,
+                         constant_term=new_constant_term)
 
 
     def add_multiple_times_row_to_row(self, coefficient, row_to_add, row_to_be_added_to):
-        pass # add your code here
+        n1 = self[row_to_add].normal_vector
+        n2 = self[row_to_be_added_to].normal_vector
+        k1 = self[row_to_add].constant_term
+        k2 = self[row_to_be_added_to].constant_term
+
+        new_normal_vector = n1.times_scalar(coefficient).plus(n2)
+        new_constant_term = (k1 * coefficient) + k2
+
+        self[row_to_be_added_to] = Plane(normal_vector=new_normal_vector,
+                                        constant_term=new_constant_term)
 
 
     def indices_of_first_nonzero_terms_in_each_row(self):
